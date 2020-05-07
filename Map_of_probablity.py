ca625bd555a44567bfbdcd798dd750939a5b5ae5
DawidKahla <51680032+DawidKahla@users.noreply.github.com>
2020-05-07 23:21:22 +0200

    Update Map_of_probablity.py
    Dodanie funkcji podstawowych

--- Map_of_probablity.py
+++ Map_of_probablity.py
@@ -36,3 +36,88 @@ def RandomCoordinate():
 
 def CalculateDistance(q1, q2):
     return np.sqrt((q1[0] - q2[0]) ** 2 + (q1[1] - q2[1]) ** 2) #liczy dystans między dwoma punktami na płaszczyźnie
+
+def AttractionForce(q, q_k):
+    return k_p * CalculateDistance(q, q_k) #zwraca sile przyciagania punktu przez punkt koncowy
+
+
+def RepulsionForceFromObstacle(q, q_oi):
+    #zwraca siłę odpychania od pojedynczej przeszkody
+    d_i = CalculateDistance(q, q_oi)                    #odległość od przeszkody
+    if (d_i < 0.01):                                    #sytuacja punktu w przeszkodzie
+        return F_rep_MaxValue
+    if (d_i < d_0):                                     #punkt w polu wpływu przeszkody
+        F_oi = k_o* (1/d_i - 1/d_0)/ d_i ** 2           #bazując na prawie Coulomba
+        if (F_oi > F_rep_MaxValue):                     #wyliczona siła nie przekroczy zadanego maksa
+            return F_rep_MaxValue
+    else:                                               #punkt poza polem wpływu przeszkody
+        F_oi = 0
+    return F_oi
+
+
+def RepulsionForcesInAPoint(q, ObstVector):
+    #zwraca siłę odpychania punktu przez wszystkie przeszkody
+    F_rep = 0
+    for q_oi in ObstVector:
+        F_rep += RepulsionForceFromObstacle(q, q_oi)  #sumujemy siły odpychania dla każdej przeszkody
+    return F_rep
+
+
+def ForcesInAPoint(WhichForce, q, q_k, ObstVector):
+    return {
+        'N': AttractionForce(q, q_k) + RepulsionForcesInAPoint(q, ObstVector), #net force, czyli siła wypadkowa
+        'A': AttractionForce(q, q_k),                                          #siła przyciągania
+        'R': RepulsionForcesInAPoint(q, ObstVector)                            #siła odpychania
+    }[WhichForce]
+
+
+def ForceVectorComponents(q, SceneObject, isElementObstacle):
+
+    if isElementObstacle:
+        LocalObstacle = (SceneObject[0] - q[0], SceneObject[1] - q[1])
+        if LocalObstacle[0] != 0:  # funkcja liniowa
+            a1 = LocalObstacle[1] / LocalObstacle[0]  # wspolczynnik funkcji y = a1*x
+            ForceSquared = RepulsionForceFromObstacle(q, SceneObject) ** 2
+            X_Distance = np.sqrt(ForceSquared / (a1 ** 2 + 1))
+            if LocalObstacle[0] < 0:  # na lewo od q
+                X_force = X_Distance
+            else:  # na prawo od q
+                X_force = -X_Distance
+            Y_force = a1 * X_force
+        else:  # wyjątek x = 0
+            X_force = 0
+            Y_force = -RepulsionForceFromObstacle(q, SceneObject)
+
+    else:  # nie przeszkoda, czyli meta
+        LocalFinish = (SceneObject[0] - q[0], SceneObject[1] - q[1])
+        if LocalFinish[0] != 0:  #funkcja liniowa
+            a1 = LocalFinish[1] / LocalFinish[0]  # wspolczynnik funkcji y = a1*x
+            ForceSquared = AttractionForce(q, SceneObject) ** 2
+            X_Distance = np.sqrt(ForceSquared / (a1 ** 2 + 1))
+            if LocalFinish[0] < 0:  # na lewo od q
+                X_force = - X_Distance
+            else:  # na prawo od q
+                X_force = X_Distance
+            Y_force = a1 * X_force
+        else:  # wyjątek x = 0
+            X_force = 0
+            Y_force = AttractionForce(q, SceneObject)
+
+    return (X_force, Y_force)
+
+
+def ResultantForceComponents(ListOfVectors):
+    #sumuje wektory w jeden
+    X_sum = Y_sum = 0
+    for SingleVector in ListOfVectors:
+        X_sum += SingleVector[0]
+        Y_sum += SingleVector[1]
+    return (X_sum, Y_sum)
+
+
+def ValueInRange(Min, Value, Max):
+    #czy wartosc zawiera sie w zadanym przedziale (wlacznie z gornym ograniczeniem)
+    if Min < Value and Value <= Max:
+        return True
+    else:
+        return False
